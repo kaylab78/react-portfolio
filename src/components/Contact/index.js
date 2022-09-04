162413a53a32a1550e90b1a398df2020ce32925e
kaylab78 <kaylabackus78@gmail.com>
2022-09-04 16:27:39 -0500

    Edited Contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
-  return (
-    <section>
-      <h1 id="contact">Contact Me</h1>
-      <form>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </section>
+    const [formState, setFormState] = useState({ name:'', email:'', message:'' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!errorMessage) {
+            setFormState({ [e.target.name]: e.target.value });
+            console.log('Form', formState);
+        }
+    };
+
+    return (
+        <section>
+            <h1 id="contact">Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" defaultValue={name} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" defaultValue={email} name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} rows="5" />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p>{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
   );
 }
 
