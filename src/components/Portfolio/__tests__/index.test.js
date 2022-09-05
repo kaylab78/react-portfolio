e2ea5d3aedd1aa1bdd24eea96955b917c49bdda6
kaylab78 <kaylabackus78@gmail.com>
2022-09-05 10:26:09 -0500

    Wrote Portfolio tests.

---
+++ src/components/Portfolio/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Portfolio from '..';
+
+afterEach(cleanup);
+
+describe('Portfolio component', () => {
+    it('renders', () => {
+        render(<Portfolio />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Portfolio />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
