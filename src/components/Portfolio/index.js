7c782e5ab4ac59d81f816c6e6281dcb2b91e8a92
kaylab78 <kaylabackus78@gmail.com>
2022-09-04 20:23:32 -0500

    Created Project component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Project from '../Project';
 
 function Portfolio() {
+    const [apps] = useState([
+        {
+            name: 'Parkour app homepage',
+            appUrl: 'https://parkour-review.herokuapp.com/',
+            repoUrl: 'https://github.com/kaylab78/playground-rating-app',
+            asset: '0-parkour.png'
+        },
+        {
+            name: 'Social network api tests',
+            appUrl: 'https://youtu.be/eYGa59kQyzg',
+            repoUrl: 'https://github.com/kaylab78/social-network-api',
+            asset: '1-social-api.png'
+        },
+        {
+            name: 'Tech blog homepage',
+            appUrl: 'https://agile-headland-59274.herokuapp.com/',
+            repoUrl: 'https://github.com/kaylab78/mvc-tech-blog',
+            asset: '2-tech-blog.png'
+        },
+        {
+            name: 'E-commerce backend tests',
+            appUrl: 'https://drive.google.com/file/d/1cxtB_hTSnTHoa6z3OZ4_vcVrn0AR1BPO/view',
+            repoUrl: 'https://github.com/kaylab78/ecommerce-backend',
+            asset: '3-ecommerce-backend.png'
+        },
+        {
+            name: 'Weather dashboard of Minneapolis forecast',
+            appUrl: 'https://kaylab78.github.io/weather-dashboard/',
+            repoUrl: 'https://github.com/kaylab78/weather-dashboard',
+            asset: '4-weather-dashboard.png'
+        },
+        {
+            name: 'Note taker app homepage',
+            appUrl: 'https://note-taker-20220704.herokuapp.com/',
+            repoUrl: 'https://github.com/kaylab78/note-taker',
+            asset: '5-note-taker.png'
+        }
+    ]);
+
     return (
         <section>
         <h1>Portfolio</h1>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,44 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Project = ({ fileName }) => {
-    const [apps] = useState([
-        {
-            name: 'Parkour app homepage',
-            appUrl: 'https://parkour-review.herokuapp.com/',
-            repoUrl: 'https://github.com/kaylab78/playground-rating-app'
-        },
-        {
-            name: 'Social network api tests',
-            appUrl: 'https://youtu.be/eYGa59kQyzg',
-            repoUrl: 'https://github.com/kaylab78/social-network-api'
-        },
-        {
-            name: 'Tech blog homepage',
-            appUrl: 'https://agile-headland-59274.herokuapp.com/',
-            repoUrl: 'https://github.com/kaylab78/mvc-tech-blog'
-        },
-        {
-            name: 'E-commerce backend tests',
-            appUrl: 'https://drive.google.com/file/d/1cxtB_hTSnTHoa6z3OZ4_vcVrn0AR1BPO/view',
-            repoUrl: 'https://github.com/kaylab78/ecommerce-backend'
-        },
-        {
-            name: 'Weather dashboard of Minneapolis forecast',
-            appUrl: 'https://kaylab78.github.io/weather-dashboard/',
-            repoUrl: 'https://github.com/kaylab78/weather-dashboard'
-        },
-        {
-            name: 'Note taker app homepage',
-            appUrl: 'https://note-taker-20220704.herokuapp.com/',
-            repoUrl: 'https://github.com/kaylab78/note-taker'
-        }
-    ]);
+const Project = ({ project }) => {
+    const { name, appUrl, repoUrl, asset } = project
 
     return (
-        <div>
-            <div>
-                
-            </div>
+        <div key={name}>
+            <img src={require(`../../assets/projects/${asset}`)} alt={name} />
+            <p><a href={repoUrl}>GitHub</a> | <a href={appUrl}>Deployed Project</a></p>
         </div>
     );
 };
