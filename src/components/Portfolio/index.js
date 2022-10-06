a4bd51e675105735fc0ecba1faf01b336ed0f968
kaylab78 <kaylabackus78@gmail.com>
2022-10-05 20:00:20 -0500

    Renamed photo files

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,31 +13,31 @@ function Portfolio() {
             name: 'Parkour app homepage',
             appUrl: 'https://parkour-review.herokuapp.com/',
             repoUrl: 'https://github.com/kaylab78/playground-rating-app',
-            asset: '0-parkour.png'
+            asset: '1-parkour.png'
         },
         {
             name: 'Social network api tests',
             appUrl: 'https://youtu.be/eYGa59kQyzg',
             repoUrl: 'https://github.com/kaylab78/social-network-api',
-            asset: '1-social-api.png'
+            asset: '2-social-api.png'
         },
         {
             name: 'Tech blog homepage',
             appUrl: 'https://agile-headland-59274.herokuapp.com/',
             repoUrl: 'https://github.com/kaylab78/mvc-tech-blog',
-            asset: '2-tech-blog.png'
+            asset: '3-tech-blog.png'
         },
         {
             name: 'E-commerce backend tests',
             appUrl: 'https://drive.google.com/file/d/1cxtB_hTSnTHoa6z3OZ4_vcVrn0AR1BPO/view',
             repoUrl: 'https://github.com/kaylab78/ecommerce-backend',
-            asset: '3-ecommerce-backend.png'
+            asset: '4-ecommerce-backend.png'
         },
         {
             name: 'Weather dashboard of Minneapolis forecast',
             appUrl: 'https://kaylab78.github.io/weather-dashboard/',
             repoUrl: 'https://github.com/kaylab78/weather-dashboard',
-            asset: '4-weather-dashboard.png'
+            asset: '5-weather-dashboard.png'
         }
     ]);
 
