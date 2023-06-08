5acebb2380fcf9d827580f75d8b52e37f8a9ac0d
kaylab78 <kaylabackus78@gmail.com>
2023-06-08 13:50:59 -0500

    Edited project link classes and targets

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -7,7 +7,7 @@ const Work = ({ work }) => {
         <div key={name} className="project-col">
             <img className="project-img" src={require(`../../assets/work/${asset}`)} alt={name} />
             <p className="project-title"><strong>{name}</strong></p>
-            <a href={url}>View Website</a>
+            <a href={url} target="_blank" className="project-link">View Website</a>
             <p>{description}</p>
             <p>Technologies: {tech}</p>
         </div>
