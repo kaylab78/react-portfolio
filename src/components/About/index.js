a2aa55524be09091bbe513f02c0747159d0debab
kaylab78 <kaylabackus78@gmail.com>
2023-06-08 15:03:28 -0500

    Edit column width on About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,16 +4,18 @@ import headShot from '../../assets/headshot-400x400.png';
 function About() {
     return (
         <section className="about-flex">
-            <div>
+            <div className="full-width">
                 <h1 id="about">About Me</h1>
+            </div>
+            <div className="half-width">
                 <img src={headShot} style={{ width: "20%" }} alt="smiling woman" />
             </div>
-            <div>
+            <div className="half-width">
                 <p>I’m a full-stack web developer certified through the University of Minnesota. I completed a six month coding boot camp on October 6, 2022. Through the curriculum I developed skills with HTML, CSS, JavaScript, APIs, and React. I also worked with MySQL and GraphQL.</p>
                 <p>I’m looking to grow my skill set and be part of a team with a diverse knowledge base. I value hard work, reliability and clear communication. I admire organizations that are invested in their team members as much as they are focused on their goals.</p>
                 <p>Prior to beginning the coding boot camp on April 21, 2022, I worked in digital marketing. I have seven years of professional experience in traditional and digital marketing. I received a bachelor of arts in media communications from North Central University in Minneapolis, Minnesota in 2015. I’m passionate about web development, which is why I made a career change.</p>
             </div>
-            <div>
+            <div className="half-width">
                 <h3>Experience</h3>
                 <p>Full-Stack Web Engineer<br/>
                 HighPower Data Solutions • Minneapolis, MN<br/>
@@ -36,7 +38,7 @@ function About() {
                 July 2015 - Aug. 2016
                 </p>
             </div>
-            <div>
+            <div className="half-width">
                 <h3>Certificate</h3>
                 <p>Full Stack Web Development Certificate<br/>
                 University of Minnesota • Minneapolis, MN<br/>
