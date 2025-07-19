71b28c8f8f686f84edd9d621d682f23e1d3b5cbc
kaylab78 <kaylabackus78@gmail.com>
2025-07-19 18:28:09 -0500

    Remove old resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 function Resume() {
     return (
         <section>
-            <h1 id="resume">Resume</h1>
-            <a href="https://drive.google.com/file/d/1gpUjajV0Jw6UU7kf0Xb-MQ--5O3nEnri/view?usp=sharing" target="_blank" rel="noreferrer" className="project-link">Download my resume.</a>
             <h2>Skills</h2>
             <ul>
                 <li>HTML5</li>
